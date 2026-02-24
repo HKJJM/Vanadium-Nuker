d8b0a7a4a1c6e479f7aec4ae00885af5a84904de
RussianHarvey <kenanhelloworld7788@gmail.com>
2026-02-25 01:09:26 +0300

    Delete requirements.bat

--- requirements.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Updating pip...
-python -m pip install --upgrade pip
-
-echo Installing required packages...
-pip install "httpx[http2]" discord.py colorama
-
-echo All packages installed!
-pause
